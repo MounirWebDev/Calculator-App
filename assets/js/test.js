185e977768b231d678109193100f6e6a9c72402f
MounirDevWev <mounirabcire@gmail.com>
2023-02-15 19:44:38 +0100

    update file: rewriting the code more clearly

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,7 +1,74 @@
 const key = document.querySelectorAll('.key')
 const screen = document.querySelector('.screen')
-const handleMath = ()=>{
-    console.log('op');
+const calculate = {
+    operands: {
+        firstOpd: null,
+        secondOpd: null
+    },
+    operators: {
+        firstOpt: null,
+        secondOpt: null
+    },
+    startCalculate: {
+        result: function () {
+
+        }
+    }
+}
+let { firstOpd, secondOpd } = calculate.operands
+let { firstOpt, secondOpt } = calculate.operators
+// c variable for counting how many times we pressed equal
+let c = 0;
+const displayResult = () => {
+    if (secondOpd && c === 0) {
+        firstOpd = secondOpd
+        c++;
+    }
+    firstOpd === null ? firstOpd = screen.textContent : secondOpd = screen.textContent
+    switch (firstOpt) {
+        case 'plus':
+            screen.innerHTML = Number(firstOpd) + Number(secondOpd)
+            break;
+        case 'minus':
+            screen.innerHTML = Number(firstOpd) - Number(secondOpd)
+            break;
+        case 'times':
+            screen.innerHTML = Number(firstOpd) * Number(secondOpd)
+            break;
+        case 'divide':
+            screen.innerHTML = Number(firstOpd) / Number(secondOpd)
+            break;
+    }
+    console.log(firstOpd);
+    console.log(firstOpt);
+    console.log(secondOpd);
+    console.log(secondOpt);
+}
+const handleMath = (btnContent) => {
+    firstOpd === null ? firstOpd = screen.textContent : secondOpd = screen.textContent
+    screen.innerHTML = ''
+    switch (btnContent) {
+        case '+':
+            if (firstOpt) {
+                firstOpd = Number(firstOpd) + Number(secondOpd)
+                secondOpd = null
+            }
+            firstOpt = 'plus'
+            break;
+        case '-':
+            firstOpt === null ? firstOpt = 'minus' : secondOpt = 'minus'
+            break;
+        case '×':
+            firstOpt === null ? firstOpt = 'times' : secondOpt = 'times'
+            break;
+        case '÷':
+            firstOpt === null ? firstOpt = 'divide' : secondOpt = 'divide'
+            break;
+    }
+    console.log(firstOpd);
+    console.log(firstOpt);
+    console.log(secondOpd);
+    console.log(secondOpt);
 }
 const displayScreen = (btnContent) => {
     const firstChar = screen.textContent.charAt(0);
@@ -21,11 +88,15 @@ const handleSymbol = (btnContent) => {
     switch (btnContent) {
         case 'reset':
             screen.innerHTML = '0'
+            firstOpd = secondOpd = firstOpt = secondOpt = null
+            c = 0
+            console.clear();
             break;
         case 'del':
             const firstChar = screen.innerHTML.charAt(0)
+            const secondChar = screen.innerHTML.charAt(1)
 
-            if (firstChar === '0') {
+            if (firstChar === '0' && secondChar !== '.') {
                 screen.innerHTML = '0';
             } else {
                 screen.innerHTML = screen.innerHTML.substring(0, screen.innerHTML.length - 1)
@@ -38,9 +109,15 @@ const handleSymbol = (btnContent) => {
         case '+':
         case '÷':
         case '-':
-            handleMath();
-        break;
-        
+            handleMath(btnContent)
+            break;
+        case '=':
+            displayResult()
+            console.log(firstOpd);
+            console.log(firstOpt);
+            console.log(secondOpd);
+            console.log(secondOpt);
+            break;
     }
 }
 
@@ -52,5 +129,3 @@ key.forEach((btn) => {
         isFinite(btnContent) ? displayScreen(btnContent) : handleSymbol(btnContent);
     })
 })
-
-// problem: when i write a float number and press the del button the screen will  return 0 
